d75ef586d26fa2b999bfd3f0479a1673c6d0c62e
Jake Ruth <jacobruth14@gmail.com>
2025-06-20 21:39:48 -0400

    wip: add yazi

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -22,7 +22,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.relativenumber = true
 vim.opt.number = true
-
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Setup lazy.nvim
 require("lazy").setup({
